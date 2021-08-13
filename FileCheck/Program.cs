0831f93cdef9ef37478bd33af2be8236e609aca2
gitgeun25 <rlaehrms7@gmail.com>
2021-08-13 19:29:28 +0900

    filecheck

---
+++ FileCheck/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("검색 경로를 입력하세요: ");
+            string result1 = Console.ReadLine();
+            Console.WriteLine("파일 확장자를 입력하세요: ");
+            string result2 = Console.ReadLine();
+
+            FileHandle file_h = new FileHandle();
+            file_h.FileCheck(result1, result2);
+        }
+    }
+
+    class FileHandle
+    {
+        public void FileCheck(string path, string file)
+        {
+            string[] files = Directory.GetFiles(path, $"*.{file}", SearchOption.AllDirectories);
+
+            Console.WriteLine("--------------------------" + Environment.NewLine);
+            foreach (string s in files)
+            {
+                FileInfo file_info = new FileInfo(s);
+                if (file_info.Length > 10000)
+                {
+                    Console.WriteLine(file_info.Name);
+                    Console.WriteLine(file_info.DirectoryName);
+                    Console.WriteLine(file_info.Length);
+                }                
+            }
+            Console.Read();
+        }
+    }
+}
