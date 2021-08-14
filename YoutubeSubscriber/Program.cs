8f9701237a37818162559c1227bffde659d7a779
gitgeun25 <rlaehrms7@gmail.com>
2021-08-14 18:32:55 +0900

    .

--- YoutubeSubscriber/Program.cs
+++ YoutubeSubscriber/Program.cs
@@ -12,7 +12,17 @@ namespace YoutubeSubscriber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("채널 주소를 입력하세요.");
+            string churl = Console.ReadLine();
+            Console.WriteLine("구독자 조회 프로그램을 실행합니다." + Environment.NewLine + Environment.NewLine);
+            
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = churl;
+            string cnt = driver.FindElement(By.Id("subscriber-count")).Text;
+            driver.Close();
 
+            Console.WriteLine();
+            Console.WriteLine(DateTime.Now + $"구독자 수는 {cnt}명 입니다.");
         }
     }
 }
