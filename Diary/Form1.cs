ccf11c386f1dbaa6c8bc0dac1a77c66a439c3f56
gitgeun25 <rlaehrms7@gmail.com>
2021-08-14 12:33:01 +0900

    .

---
+++ Diary/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(@"C:\Users\jin yeong\.vscode\csharp_project\Diary\" + textBox1.Text + ".txt", 
+                textBox2.Text + Environment.NewLine + Environment.NewLine + "현재시간 : " + DateTime.Now);
+            MessageBox.Show("일기 내용이 입력되었습니다.");
+        }
+    }
+}
---
+++ LoginForm/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_btn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jin yeong\Documents\Data5.mdf;Integrated Security=True;Connect Timeout=30");
+
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) from USERINFO where USERNAME='"+ID_txt.Text+"' and PASSWORD = '"+PW_txt.Text+"'", con);
+
+            DataTable newTable = new DataTable();
+
+            sda.Fill(newTable);
+
+            if(newTable.Rows[0][0].ToString() == "1")
+            {
+                // 로그인 성공
+                this.Hide();
+
+                MainForm mainform = new MainForm();
+                mainform.Show();
+            }
+            else
+            {
+                // 로그인 실패
+                MessageBox.Show("아아디와 비밀번호를 확인해주세요.");
+            }           
+        }
+    }
+}
