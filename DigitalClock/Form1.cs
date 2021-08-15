f286e182323555681d664f3d09267a5102f9002e
gitgeun25 <rlaehrms7@gmail.com>
2021-08-15 18:18:20 +0900

    .

---
+++ DigitalClock/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Enabled = true;          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+            if (DateTime.Now.Hour == 14 && DateTime.Now.Minute == 27 && DateTime.Now.Second == 00)
+                PlaySound();
+        }
+
+        private void PlaySound()
+        {
+            SoundPlayer sound = new SoundPlayer(@"C:\Users\jin yeong\Desktop\01. 그대여 그대여 나의 그대여.wav");
+            sound.Play();
+        }
+    }
+}
---
+++ StudentsManagement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 학생 정보 입력(이름, 학년, 반, 주소, 취미)
+// 학생 성적 입력(국, 영, 수)
+// 학생 정보 엑셀에 저장
+// 학생 정보 엑셀에 저장시 학생 번호 순서별로 자동 생성하여 엑셀에 저장
+// 학생 정보 엑셀에서 가져오기
+// 학생 시험 석차순 정렬하여 출력
+// 학생 정보 전체 출력
+// 학생 정보 번호순으로 검색하여 출력
+
+// 데이터베이스가 없는 경우
+
+namespace StudentsManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
