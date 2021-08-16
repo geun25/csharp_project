c5751a5bbb123c8bd8b18345539be3c4e7f592a4
gitgeun25 <rlaehrms7@gmail.com>
2021-08-16 11:45:51 +0900

    .

---
+++ ProcessKill/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace ProcessKill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(53, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "프로세스 이름";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(308, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 52);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "프로세스 CPU %";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(562, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 52);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "실행 결과";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 504);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "프로세스 체크 시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 504);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 51);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "프로세스 체크 종료";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(677, 504);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 51);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "종료";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 126);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 343);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(308, 126);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 343);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(562, 126);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(211, 343);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(562, 502);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(95, 53);
+            this.textBox4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 905);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ ProcessKill/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ProcessKill
+{
+    public partial class Form1 : Form
+    {
+        bool state = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(ProcessChecker);
+            thread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            state = false;
+            textBox3.Text = "프로세스 체크가 종료되었습니다.";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process[] process = new Process[5];
+            process = Process.GetProcessesByName(textBox4.Text);
+            process[0].Kill();
+            textBox3.Text = $"{textBox4.Text} 프로세스가 종료되었습니다.";
+        }
+
+        void ProcessChecker()
+        {
+            PerformanceCounter[] counters = new PerformanceCounter[textBox1.Lines.Length];
+            for (int i = 0; i < counters.Length; i++)
+            {
+                counters[i] = new PerformanceCounter("Process", "% User Time", textBox1.Text);
+            }
+
+            while(state)
+            {
+                if (InvokeRequired) // 컨트롤이 메인 스레드에서 만들어졌고, CPU 사용률 체크는 별도 스레드에서 수행되기 때문에 메서드를 호출해야하는 상황 발생.
+                                    // Invoke가 필요하면 true값 반환
+                {
+                    label2.BeginInvoke(new Action(() =>
+                    {
+                        try
+                        {
+                            if (textBox1.Lines.Length == 1)
+                            {
+                                textBox2.Text = counters[0].NextValue().ToString() + "%" + Environment.NewLine;
+                            }
+                            else if (textBox1.Lines.Length == 2)
+                            {
+                                textBox2.Text = counters[0].NextValue().ToString() + "%" + Environment.NewLine
+                                + counters[1].NextValue().ToString() + "%";
+                            }
+                            else if (textBox1.Lines.Length == 3)
+                            {
+                                textBox2.Text = counters[0].NextValue().ToString() + "%" + Environment.NewLine
+                                + counters[1].NextValue().ToString() + "%" + Environment.NewLine
+                                + counters[2].NextValue().ToString() + "%";
+                            }
+                        }
+
+                        catch
+                        {
+                            textBox3.Text = "프로세스가 없습니다.";
+                        }
+                    }));                   
+                }
+                else
+                {
+                    if (textBox1.Lines.Length == 1)
+                    {
+                        textBox2.Text = counters[0].NextValue().ToString() + "%" + Environment.NewLine;
+                    }
+                    else if (textBox1.Lines.Length == 2)
+                    {
+                        textBox2.Text = counters[0].NextValue().ToString() + "%" + Environment.NewLine
+                        + counters[1].NextValue().ToString() + "%";
+                    }
+                    else if (textBox1.Lines.Length == 3)
+                    {
+                        textBox2.Text = counters[0].NextValue().ToString() + "%" + Environment.NewLine
+                        + counters[1].NextValue().ToString() + "%" + Environment.NewLine
+                        + counters[2].NextValue().ToString() + "%";
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
