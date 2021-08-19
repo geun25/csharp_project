0355fbe7aa9d08495d114b3644a6ad0dd26d99c9
geun25 <sas0898@naver.com>
2021-08-19 22:04:36 +0900

    .

---
+++ NaverCrawler/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace NaverCrawler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(292, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "네이버 창 모드 검색";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(292, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "네이버 백그라운드 검색";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(92, 437);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(292, 66);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "검색 결과 보기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(92, 538);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(292, 66);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "리스트박스 선택 초기화";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(92, 728);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(292, 66);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "모든 창 닫기";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 122);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 36);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(484, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1321, 72);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Blog";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(484, 437);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1321, 72);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Video";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(484, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1321, 292);
+            this.listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(484, 512);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(1321, 292);
+            this.listBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1821, 890);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ NaverCrawler/Form1.cs
@@ -0,0 +1,124 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace NaverCrawler
+{
+    public partial class Form1 : Form
+    {
+        string result1, result2, result3, result4, result5, result6, result7, result8;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");
+
+            IWebDriver driver = new ChromeDriver(service, options);
+            driver.Url = "https://www.naver.com";
+            driver.FindElement(By.Name("query")).SendKeys(textBox1.Text);
+            driver.FindElement(By.Id("search_btn")).Click();
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            try
+            {
+                driver.FindElement(By.Id("//*[@id=\"main_pack\"]/section[4]/div/div[1]/div/div[1]/a[2]")).Click();
+
+                listBox1.Items.Add(driver.FindElement(By.XPath("//*[@id=\"sp_blog_1\"]/div/div/a")).Text);
+                result1 = driver.FindElement(By.XPath("//*[@id=\"sp_blog_1\"]/div/div/div[2]/div/a/div")).Text;
+                listBox1.Items.Add(driver.FindElement(By.XPath("//*[@id=\"sp_blog_2\"]/div/div/a")).Text);
+                result2 = driver.FindElement(By.XPath("//*[@id=\"sp_blog_2\"]/div/div/div[2]/div/a/div")).Text;
+                listBox1.Items.Add(driver.FindElement(By.XPath("//*[@id=\"sp_blog_3\"]/div/div/a")).Text);
+                result3 = driver.FindElement(By.XPath("//*[@id=\"sp_blog_3\"]/div/div/div[2]/div/a/div")).Text;
+                listBox1.Items.Add(driver.FindElement(By.XPath("//*[@id=\"sp_blog_4\"]/div/div/a")).Text);
+                result4 = driver.FindElement(By.XPath("//*[@id=\"sp_blog_4\"]/div/div/div[2]/div/a/div")).Text;
+
+            }
+            catch { listBox1.Items.Add("블로그가 없습니다."); }
+
+            try
+            {
+                listBox1.Items.Add(driver.FindElement(By.XPath("//*[@id=\"sp_blog_1\"]/div/div/a")).Text);
+                result5 = driver.FindElement(By.XPath("//*[@id=\"sp_blog_1\"]/div/div/div[2]/div/a/div")).Text;
+                listBox1.Items.Add(driver.FindElement(By.XPath("//*[@id=\"sp_blog_2\"]/div/div/a")).Text);
+                result6 = driver.FindElement(By.XPath("//*[@id=\"sp_blog_2\"]/div/div/div[2]/div/a/div")).Text;
+                listBox1.Items.Add(driver.FindElement(By.XPath("//*[@id=\"sp_blog_3\"]/div/div/a")).Text);
+                result7 = driver.FindElement(By.XPath("//*[@id=\"sp_blog_3\"]/div/div/div[2]/div/a/div")).Text;
+                listBox1.Items.Add(driver.FindElement(By.XPath("//*[@id=\"sp_blog_4\"]/div/div/a")).Text);
+                result8 = driver.FindElement(By.XPath("//*[@id=\"sp_blog_4\"]/div/div/div[2]/div/a/div")).Text;
+            }
+
+            catch { listBox2.Items.Add("동영상이 없습니다."); }
+            driver.Close();               
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            IWebDriver driver = new ChromeDriver(service);
+
+            if(listBox1.SelectedItem != null && listBox2.SelectedItem != null)
+            {
+                if (listBox1.SelectedIndex.Equals(0)) driver.Url = "https://" + result1;
+                else if (listBox1.SelectedIndex.Equals(1)) driver.Url = "https://" + result2;
+                else if (listBox1.SelectedIndex.Equals(2)) driver.Url = "https://" + result3;
+                else if (listBox1.SelectedIndex.Equals(3)) driver.Url = "https://" + result4;               
+            }
+
+            else if (listBox2.SelectedItem != null && listBox1.SelectedItem != null)
+            {
+                if (listBox1.SelectedIndex.Equals(0)) driver.Url = "https://" + result5;
+                else if (listBox2.SelectedIndex.Equals(1)) driver.Url = "https://" + result6;
+                else if (listBox2.SelectedIndex.Equals(2)) driver.Url = "https://" + result7;
+                else if (listBox2.SelectedIndex.Equals(3)) driver.Url = "https://" + result8;
+            }
+
+            if (driver.Manage().Window == null) driver.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedItem = null;
+            listBox2.SelectedItem = null;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process[] process = new Process[100];
+                process = Process.GetProcessesByName("chrome");
+                foreach (Process x in process) x.Kill();
+                process = Process.GetProcessesByName("chromedriver");
+                foreach (Process x in process) x.Kill();
+                process = Process.GetProcessesByName("conhost");
+                foreach (Process x in process) x.Kill();
+            }
+            catch { }
+        }
+    }
+}
